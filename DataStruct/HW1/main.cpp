8dff659ae802a68dcce8924660782f88b8fe190f
Fans3267 <fanfan22299999@gmail.com>
2024-11-28 22:31:24 +0800

    HW1 done

--- DataStruct/HW1/main.cpp
+++ DataStruct/HW1/main.cpp
@@ -8,7 +8,8 @@ private:
     vector<vector<int>> data;
 public:
     void InsertMaze(int len,int wid);
-    void findpath(int len,int wid);
+    bool findpath_method1(int len,int wid);
+    void findpath_method2(int len,int wid);
 };
 void mazeproblem::InsertMaze(int len,int wid){
     //cout<<"input maze data: ";
@@ -24,8 +25,8 @@ void mazeproblem::InsertMaze(int len,int wid){
     }
 }
 
-
-void mazeproblem::findpath(int len,int wid){
+/*//abandoned
+void mazeproblem::findpath_method(int len,int wid){
     //char dir[8]={'n','ne','e','se','s','sw','w','nw'};
     int dir[8][2]={{-1,0},{-1,1},{0,1},{1,1},{1,0},{1,-1},{0,-1},{-1,-1}};
     int step=0,branch[8];
@@ -104,18 +105,119 @@ void mazeproblem::findpath(int len,int wid){
         }
     }
 }
+*/
 
+bool mazeproblem::findpath_method1(int len, int wid) {
+    int dir[2][2] = {{0,1},{1,0}};
+    bool visited[len][wid] = {false};
+    vector<vector<int>> path;
+
+    struct Current {int x,y;};
+    struct Next {int x,y;};
+    Next next;
+    Current current = {0,0};
+    visited[0][0] = true;
+    path.push_back({current.x, current.y});
+
+    //the actual working part idk ffs, fml im gonna kms
+    while (!path.empty()) {
+        int x=current.x;
+        int y=current.y;
+        //ends
+        if (x==len-1 && y==wid-1) {
+            cout<<"\n"<<"直角方法: "<<path.size()<<"點\n";
+            for (int i=path.size();i>0;i--) {
+                cout<<"("<<path[i-1][0]+ 1<<","<<path[i-1][1]+ 1<<")";
+                if (i>1)cout<<"<-";
+            }
+            cout<<"\n\n";
+            return true;
+        }
+
+        //move
+        bool moved = false;
+        for (int i=0;i<2;i++) {
+            next.x = x+dir[i][0];
+            next.y = y+dir[i][1];
+            if (next.x >= 0 && next.y >= 0 && next.x < len && next.y < wid && !visited[next.x][next.y] && data[next.x][next.y] == 0) {
+                visited[next.x][next.y]=true;
+                path.push_back({next.x,next.y});
+                current={next.x,next.y};
+                moved=true;
+                break;
+            }
+        }
+        if (!moved) {
+            path.pop_back();
+            if (!path.empty()) {
+                current.x=path.back()[0];
+                current.y=path.back()[1];
+            }
+        }
+    }
+    return false;
+}
+
+void mazeproblem::findpath_method2(int len, int wid) {
+    int dir[8][2] = {{-1,0}, {-1,1}, {0,1}, {1,1}, {1,0}, {1,-1}, {0,-1}, {-1,-1}};
+    bool visited[len][wid] = {false};
+    vector<vector<int>> path;
+
+    struct Current {int x,y;};
+    struct Next {int x,y;};
+    Next next;
+    Current current = {0,0};
+    visited[0][0] = true;
+    path.push_back({current.x, current.y});
+
+    //the actual working part idk ffs, fml im gonna kms
+    while (!path.empty()) {
+        int x=current.x;
+        int y=current.y;
+        //ends
+        if (x==len-1 && y==wid-1) {
+            cout<<endl<<"任意方法: "<<path.size()<<"點\n";
+            for (int i=path.size();i>0;i--) {
+                cout<<"("<<path[i-1][0]+ 1<<","<<path[i-1][1]+ 1<<")";
+                if (i>1)cout<<"<-";
+            }
+            cout<<"\n\n";
+            return;
+        }
+        //move
+        bool moved = false;
+        for (int i=0;i<8;i++) {
+            next.x = x+dir[i][0];
+            next.y = y+dir[i][1];
+            if (next.x >= 0 && next.y >= 0 && next.x < len && next.y < wid && !visited[next.x][next.y] && data[next.x][next.y] == 0) {
+                visited[next.x][next.y]=true;
+                path.push_back({next.x,next.y});
+                current={next.x,next.y};
+                moved=true;
+                break;
+            }
+        }
+        if (!moved) {
+            path.pop_back();
+            if (!path.empty()) {
+                current.x=path.back()[0];
+                current.y=path.back()[1];
+            }
+        }
+    }
+    cout<<endl<<"沒路\n\n";
+}
 int main(){
-    cout<<"input:";
+    cout<<"(Paste And Enter) Input:\n";
     int amount=0,len=0,wid=0;
     cin>>amount;
-    
-    for (int i=0;i<amount;i++)
-    {
+
+    for (int i=0;i<amount;i++){
         cin>>len>>wid;
         mazeproblem maze;
         maze.InsertMaze(len,wid);
-        maze.findpath(len,wid);
+        if (!maze.findpath_method1(len,wid))
+            maze.findpath_method2(len,wid);
     }
 }
 
